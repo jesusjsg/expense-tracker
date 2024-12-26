e96f8e78940fdff10b4fd7428ece209c2cd08f67
jesus <jesusjsg@proton.me>
2024-12-26 12:12:37 -0400

    fix: webpack encore fixed for error '_default not configured'

--- config/container/container_bindings.php
+++ config/container/container_bindings.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 use App\Config;
 use App\Enum\AppEnvironment;
+use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 use Psr\Container\ContainerInterface;
@@ -12,9 +13,13 @@ use Symfony\Bridge\Twig\Extension\AssetExtension;
 use Symfony\Component\Asset\Package;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollectionInterface;
 use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
 use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
+use Twig\Extension\DebugExtension;
 use Twig\Extra\Intl\IntlExtension;
 
 use function DI\create;
@@ -24,12 +29,13 @@ return [
     EntityManager::class   => function (Config $config) {
         $connectionParams = $config->get('doctrine.connection');
 
+        $connection = DriverManager::getConnection($connectionParams);
+
         $config = ORMSetup::createAttributeMetadataConfiguration(
             $config->get('doctrine.entity_dir'),
             $config->get('doctrine.dev_mode')
         );
-        $entityManager = new EntityManager($connectionParams, $config);
-
+        $entityManager = new EntityManager($connection, $config);
         return $entityManager;
     },
         
@@ -42,17 +48,26 @@ return [
         $twig->addExtension(new IntlExtension());
         $twig->addExtension(new EntryFilesTwigExtension($container));
         $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
+        $twig->addExtension(new DebugExtension());
         
         return $twig;
     },
 
     /* The following two bindings are needed for EntryFilesTwigExtension and AssetExtension to work for Twig */
 
+    'webpack_encore.entrypoint_lookup_collection' => static function (): EntrypointLookupCollectionInterface {
+        $entrypointLookup = new EntrypointLookup(BUILD_PATH . '/entrypoints.json');
+        $serviceLocator =  new ServiceLocator(['_default' => function () use ($entrypointLookup) {
+            return $entrypointLookup;
+        }]);
+        return new EntrypointLookupCollection($serviceLocator);
+    },
+
     'webpack_encore.packages' => fn() => new Packages(
         new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
     ),
     'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
-        new EntrypointLookupCollectionInterface(BUILD_PATH . '/entrypoints.json'),
+        $container->get('webpack_encore.entrypoint_lookup_collection'),
         $container->get('webpack_encore.packages')
     )
 ];
