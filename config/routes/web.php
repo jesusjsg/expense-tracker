b8597c6e4f4a2333388e88219c94140854e69abd
jesus <jesusjsg@proton.me>
2024-12-26 12:20:43 -0400

    fix: index added to same HomeController array

--- config/routes/web.php
+++ config/routes/web.php
@@ -6,5 +6,5 @@ use App\Controllers\HomeController;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class], 'index');
+    $app->get('/', [HomeController::class, 'index']);
 };
