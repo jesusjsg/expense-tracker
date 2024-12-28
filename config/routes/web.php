0d477a28dd8853956a01200e125fbae70539fa8c
jesus <jesusjsg@proton.me>
2024-12-28 14:34:12 -0400

    feat: Guest and Auth middleware set in necesary routes

--- config/routes/web.php
+++ config/routes/web.php
@@ -4,12 +4,17 @@ declare(strict_types = 1);
 
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
+use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class, 'index']);
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->get('/signup', [AuthController::class, 'signupView']);
-    $app->post('/login', [AuthController::class, 'login']);
-    $app->post('/signup', [AuthController::class, 'signup']);
+    $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
+
+    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestMiddleware::class);
+    $app->get('/signup', [AuthController::class, 'signupView'])->add(GuestMiddleware::class);
+    
+    $app->post('/login', [AuthController::class, 'login'])->add(GuestMiddleware::class);
+    $app->post('/signup', [AuthController::class, 'signup'])->add(GuestMiddleware::class);
+    $app->post('/logout', [AuthController::class, 'logout'])->add(AuthMiddleware::class);
 };
