98a1bc45f96b3f811c8ab6f618cee6717e9d9618
jesus <jesusjsg@proton.me>
2024-12-28 11:44:54 -0400

    feat: exclude sensitive data

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -23,8 +23,13 @@ class ValidationExceptionMiddleware implements MiddlewareInterface
             return $handler->handle($request);
         } catch(ValidationException $error) {
             $response = $this->responseFactory->createResponse();
+            $referer  = $request->getServerParams()['HTTP_REFERER'];
+            $oldData = $request->getParsedBody();
 
-            $referer = $request->getServerParams()['HTTP_REFERER'];
+            $excludeFields = ['password', 'confirmPassword'];
+
+            $_SESSION['errors'] = $error->errors;
+            $_SESSION['old'] = array_diff_key($oldData, array_flip($excludeFields));
 
             return $response->withHeader('Location', $referer)->withStatus(302);
         }
