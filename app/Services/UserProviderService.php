55fe40271507071d92953c37ddd22b1efc07f167
jesus <jesusjsg@proton.me>
2024-12-31 15:14:27 -0400

    feat: new method to create an user

--- app/Services/UserProviderService.php
+++ app/Services/UserProviderService.php
@@ -6,6 +6,7 @@ namespace App\Services;
 
 use App\Contracts\UserInterface;
 use App\Contracts\UserProviderServiceInterface;
+use App\DataObjects\SignupUserData;
 use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 
@@ -23,6 +24,20 @@ class UserProviderService implements UserProviderServiceInterface
     public function getByCredentials(array $data): ?UserInterface
     {
         return $this->entityManager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+    }
+
+    public function newUser(SignupUserData $data): UserInterface
+    {
+        $user = new User();
+
+        $user->setName($data->name);
+        $user->setEmail($data->email);
+        $user->setPassword(password_hash($data->password, PASSWORD_BCRYPT, ['cost' => 12]));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
     }
 
 }
