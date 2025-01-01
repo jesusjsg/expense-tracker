4e8bf71daf09caed5a96b7fe502779cc5e7df7c4
jesus <jesusjsg@proton.me>
2025-01-01 03:35:24 -0400

    feat: new template to categories view

---
+++ resources/js/categories.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.edit-category-btn').forEach(button => {
+        button.addEventListener('click', function (event) {
+            const categoryId = event.currentTarget.getAttribute('data-id')
+
+            console.log(categoryId)
+        })
+    })
+})
