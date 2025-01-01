0956659548cb9ac07ccb50a9a5cd7884080c0f61
jesus <jesusjsg@proton.me>
2025-01-01 03:36:10 -0400

    perf: new categories route and set groups for any entity

--- config/routes/web.php
+++ config/routes/web.php
@@ -3,18 +3,28 @@
 declare(strict_types = 1);
 
 use App\Controllers\AuthController;
+use App\Controllers\CategoriesController;
 use App\Controllers\HomeController;
 use App\Middleware\AuthMiddleware;
 use App\Middleware\GuestMiddleware;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
-    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestMiddleware::class);
-    $app->get('/signup', [AuthController::class, 'signupView'])->add(GuestMiddleware::class);
-    
-    $app->post('/login', [AuthController::class, 'login'])->add(GuestMiddleware::class);
-    $app->post('/signup', [AuthController::class, 'signup'])->add(GuestMiddleware::class);
+    $app->group('', function (RouteCollectorProxy $guest) {
+        $guest->get('/login', [AuthController::class, 'loginView']);
+        $guest->get('/signup', [AuthController::class, 'signupView']);
+        $guest->post('/login', [AuthController::class, 'login']);
+        $guest->post('/signup', [AuthController::class, 'signup']);
+    })->add(GuestMiddleware::class);
+
     $app->post('/logout', [AuthController::class, 'logout'])->add(AuthMiddleware::class);
+
+    $app->group('/categories', function (RouteCollectorProxy $categories) {
+        $categories->get('', [CategoriesController::class, 'index']);
+        $categories->post('', [CategoriesController::class, 'store']);
+        $categories->delete('/{id}', [CategoriesController::class, 'delete']);
+    })->add(AuthMiddleware::class);
 };
