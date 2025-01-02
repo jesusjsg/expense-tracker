3419c306ac6392918edcec639a2d6db97a1d68c1
jesus <jesusjsg@proton.me>
2025-01-02 16:17:24 -0400

    feat: some crud methods implemented miss the edit method

--- app/Controllers/CategoriesController.php
+++ app/Controllers/CategoriesController.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
+use App\Contracts\ValidatorFactoryInterface;
+use App\Services\CategoryService;
+use App\Validators\CreateCategoryValidator;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
@@ -11,22 +14,38 @@ use Slim\Views\Twig;
 class CategoriesController
 {
     public function __construct(
-        private readonly Twig $twig
+        private readonly Twig $twig,
+        private readonly ValidatorFactoryInterface $validatorFactory,
+        private readonly CategoryService $categoryService
     ) {
     }
 
     public function index(Request $request, Response $response): Response
-    {
-        return $this->twig->render($response, 'categories/index.twig');
+    {        
+        return $this->twig->render(
+            $response, 
+            'categories/index.twig',
+            [
+                'categories' => $this->categoryService->getAll(),
+            ]
+        );
     }
 
     public function store(Request $request, Response $response): Response
     {
-        return $response;
+        $data = $this->validatorFactory->make(CreateCategoryValidator::class)->validate(
+            $request->getParsedBody()
+        );
+
+        $this->categoryService->create($data['name'], $request->getAttribute('user'));
+
+        return $response->withHeader('Location', '/categories')->withStatus(302);
+
     }
 
-    public function delete(Request $request ,Response $response): Response
+    public function delete(Request $request ,Response $response, array $args): Response
     {
-        return $response;
+        $this->categoryService->delete((int) $args['id']);
+        return $response->withHeader('Location', '/categories')->withStatus(302);
     }
 }
