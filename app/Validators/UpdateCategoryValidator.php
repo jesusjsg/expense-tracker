c03a17b56327898d4b4fc9164056e6b9cbd78765
jesus <jesusjsg@proton.me>
2025-01-07 18:03:40 -0400

    feat: new validation for update category modal

---
+++ app/Validators/UpdateCategoryValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Validators;
+
+use App\Contracts\ValidatorInterface;
+use App\Exception\ValidationException;
+use Valitron\Validator;
+
+class UpdateCategoryValidator implements ValidatorInterface
+{
+    public function validate(array $data): array
+    {
+        $validator = new Validator($data);
+
+        $validator->rule('required', ['name', 'id']);
+        $validator->rule('lengthMax', 'name', 30);
+        $validator->rule('integer', 'id')
+
+        if (! $validator->validate()) {
+            throw new ValidationException($validator->errors());
+        }
+
+        return $data;
+    }
+}
