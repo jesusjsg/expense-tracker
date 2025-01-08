2e2dcc7d02b4412260e8a99dbd28448a18502583
jesus <jesusjsg@proton.me>
2025-01-07 20:33:52 -0400

    refactor: ajax method moved to ajax section to set the options

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,4 +1,5 @@
 import { Modal } from "bootstrap"
+import { get, post } from "./ajax"
 
 window.addEventListener('DOMContentLoaded', function () {
     const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
@@ -7,8 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
         button.addEventListener('click', function (event) {
             const categoryId = event.currentTarget.getAttribute('data-id')
 
-            fetch(`/categories/${categoryId}`)
-                .then(response => response.json())
+            get(`/categories/${categoryId}`)
                 .then(response => openEditCategoryModal(editCategoryModal, response))
         })
     })
@@ -16,35 +16,13 @@ window.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.save-category-btn').addEventListener('click', function (event) {
         const categoryId = event.currentTarget.getAttribute('data-id')
 
-        fetch(`/categories/${categoryId}`, {
-            method: "POST",
-            body: JSON.stringify({
-                name: editCategoryModal._element.querySelector('input[name="name"]').value,
-                ...getCsrfValues()
-            }),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        }).then(response => response.json())
-          .then(response => {
-              console.log(response)
-          })
+        post(`/categories/${categoryId}`, {
+            name: editCategoryModal._element.querySelector('input[name="name"]').value
+        }).then(response => {
+            console.log(response)
+        })
     })
 })
-
-function getCsrfValues() {
-    const csrfInputName  = document.querySelector('#csrfName')
-    const csrfInputValue = document.querySelector('#csrfValue')
-    const csrfNameKey    = csrfInputName.getAttribute('name')
-    const csrfName       = csrfInputName.content
-    const csrfValueKey   = csrfInputValue.getAttribute('name')
-    const csrfValue      = csrfInputValue.content
-
-    return {
-        [csrfNameKey]: csrfName,
-        [csrfValueKey]: csrfValue
-    }
-}
 
 function openEditCategoryModal(modal, { id, name }) {
     const nameInput = modal._element.querySelector('input[name="name"]')
