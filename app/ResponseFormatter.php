c4aa3eb36673598d4e745b1e429f7e8dc0c4c834
jesus <jesusjsg@proton.me>
2025-01-12 11:18:02 -0400

    perf: file formated

--- app/ResponseFormatter.php
+++ app/ResponseFormatter.php
@@ -12,8 +12,8 @@ class ResponseFormatter
         ResponseInterface $response, 
         array $data,
         int $flags = JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_THROW_ON_ERROR
-    ): ResponseInterface
-    {
+
+    ): ResponseInterface {
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(json_encode($data, $flags));
 
